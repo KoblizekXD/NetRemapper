b725f782ea42d958c94abe1b97652820d6f70007
KoblizekXD <janprokupek@centrum.cz>
2024-10-23 16:09:24 +0200

    Update csprojs to fix default namespaces. Cleanup code

--- NetRemapper-API/NetRemapper.cs
+++ NetRemapper-API/NetRemapper.cs
@@ -24,7 +24,7 @@ public class NetRemapper
     public Mappings? Mappings { get; private set; }
     public string DefaultNamespace { get; set; } = "obf";
     public string TargetNamespace { get; set; } = "named";
-    public bool Verbose { get; set; } = false;
+    public bool Verbose { get; init; }
 
     public bool MappingsLoaded => Mappings is not null;
 
@@ -34,7 +34,7 @@ public class NetRemapper
     }
 
     /// <summary>
-    ///     Remaps the <see cref="assemblyPath"/> according to the loaded mappings(<see cref="mappings"/>).
+    ///     Remaps the <see cref="Assembly"/> according to the loaded <see cref="Mappings"/>.
     ///     If the mappings are not loaded, an exception is thrown.
     ///     The process will use <see cref="DefaultNamespace"/> as the namespace to remap from and <see cref="TargetNamespace"/> as the namespace to remap to.
     /// </summary>
@@ -42,11 +42,11 @@ public class NetRemapper
     /// <exception cref="Exception">Thrown when no mappings are loaded.</exception>
     public void Remap(string output)
     {
-        if (Mappings is null) throw new Exception("Mappings are not loaded.");
+        if (Mappings is null) throw RemapperException.UnloadedMappings();
 
-        foreach (TypeDefinition type in Module.Types)
+        foreach (var type in Module.Types)
         {
-            if (Mappings.GetType(type.Name, DefaultNamespace) is TypeDefinitionEntry entry1)
+            if (Mappings.GetType(type.Name, DefaultNamespace) is { } entry1)
             {
                 WriteIfVerbose($"Remapping type {type.Name} -> {entry1[TargetNamespace]}");
                 type.Name = entry1.Names[TargetNamespace];
@@ -54,20 +54,18 @@ public class NetRemapper
 
             foreach (var property in type.Properties)
             {
-                if (Mappings.GetProperty(property.Name, DefaultNamespace, type.Name, DefaultNamespace) is PropertyDefinitionEntry entry)
-                {
-                    WriteIfVerbose($"Remapping property {property.Name} -> {entry[TargetNamespace]}");
-                    property.Name = entry.Names[TargetNamespace];
-                }
+                if (Mappings.GetProperty(property.Name, DefaultNamespace, type.Name, DefaultNamespace) is not 
+                    { } entry) continue;
+                WriteIfVerbose($"Remapping property {property.Name} -> {entry[TargetNamespace]}");
+                property.Name = entry.Names[TargetNamespace];
             }
 
             foreach (var field in type.Fields)
             {
-                if (Mappings.GetField(field.Name, DefaultNamespace, type.Name, DefaultNamespace) is FieldDefinitionEntry entry)
-                {
-                    WriteIfVerbose($"Remapping field {field.Name} -> {entry[TargetNamespace]}");
-                    field.Name = entry.Names[TargetNamespace];
-                }
+                if (Mappings.GetField(field.Name, DefaultNamespace, type.Name, DefaultNamespace) is not
+                    { } entry) continue;
+                WriteIfVerbose($"Remapping field {field.Name} -> {entry[TargetNamespace]}");
+                field.Name = entry.Names[TargetNamespace];
             }
 
             foreach (var method in type.Methods)
@@ -124,36 +122,33 @@ public class NetRemapper
 
     private void RemapMemberReference(MemberReference member)
     {
-        if (Mappings!.GetType(member.DeclaringType.Name, DefaultNamespace) is TypeDefinitionEntry typeDefinition)
-        {
-            WriteIfVerbose($"Remapping reference to {member.Name}'s type {member.DeclaringType.Name} -> {typeDefinition[TargetNamespace]}");
-            member.DeclaringType = new(member.DeclaringType.Namespace, typeDefinition.Names[TargetNamespace], member.DeclaringType.Module, member.DeclaringType.Scope);
-        }
+        if (Mappings!.GetType(member.DeclaringType.Name, DefaultNamespace) is not { } typeDefinition) return;
+        
+        WriteIfVerbose($"Remapping reference to {member.Name}'s type {member.DeclaringType.Name} -> {typeDefinition[TargetNamespace]}");
+        member.DeclaringType = new(member.DeclaringType.Namespace, typeDefinition.Names[TargetNamespace], member.DeclaringType.Module, member.DeclaringType.Scope);
     }
 
     private void RemapMethodReference(ILProcessor processor, Instruction instruction, MethodReference mRef)
     {
-        if (Mappings!.GetMethod(mRef.Name, DefaultNamespace, mRef.Name, TargetNamespace) is MethodDefinitionEntry methodEntry)
-        {
-            WriteIfVerbose($"Remapping method reference {mRef.Name} -> {methodEntry[TargetNamespace]}");
-            mRef.Name = methodEntry.Names[TargetNamespace];
-            processor.Replace(instruction, processor.Create(instruction.OpCode, mRef));
-        }
+        if (Mappings!.GetMethod(mRef.Name, DefaultNamespace, mRef.Name, TargetNamespace) is not { } methodEntry) return;
+        
+        WriteIfVerbose($"Remapping method reference {mRef.Name} -> {methodEntry[TargetNamespace]}");
+        mRef.Name = methodEntry.Names[TargetNamespace];
+        processor.Replace(instruction, processor.Create(instruction.OpCode, mRef));
     }
 
     private void RemapFieldReference(ILProcessor processor, Instruction instruction, FieldReference fRef)
     {
-        if (Mappings!.GetField(fRef.Name, DefaultNamespace, fRef.Name, TargetNamespace) is FieldDefinitionEntry fieldEntry)
-        {
-            WriteIfVerbose($"Remapping field reference {fRef.Name} -> {fieldEntry[TargetNamespace]}");
-            fRef.Name = fieldEntry.Names[TargetNamespace];
-            processor.Replace(instruction, processor.Create(instruction.OpCode, fRef));
-        }
+        if (Mappings!.GetField(fRef.Name, DefaultNamespace, fRef.Name, TargetNamespace) is not { } fieldEntry) return;
+        
+        WriteIfVerbose($"Remapping field reference {fRef.Name} -> {fieldEntry[TargetNamespace]}");
+        fRef.Name = fieldEntry.Names[TargetNamespace];
+        processor.Replace(instruction, processor.Create(instruction.OpCode, fRef));
     }
 
     public void ExportMappings(string output)
     {
-        if (Mappings is null) throw new Exception("Mappings are not loaded.");
+        if (Mappings is null) throw RemapperException.UnloadedMappings();
 
         MappingsWriter.Write(Mappings, output);
     }
--- NetRemapper-API/RemapperException.cs
+++ NetRemapper-API/RemapperException.cs
@@ -1,13 +1,10 @@
-﻿
-namespace NetRemapper;
+﻿namespace NetRemapper;
 
-
-[Serializable]
 public class RemapperException : Exception
 {
 	public RemapperException() { }
 	public RemapperException(string message) : base(message) { }
 	public RemapperException(string message, Exception inner) : base(message, inner) { }
-
-	public static RemapperException UnloadedMappings() => new RemapperException("Mappings are not loaded.");
+	
+	public static RemapperException UnloadedMappings() => new("Mappings are not loaded.");
 }
